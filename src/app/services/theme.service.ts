967611d639f461367af8a7e6efd0b3ad5d151a7e
Miro Manestar <kinectechx@gmail.com>
2019-10-04 11:49:09 -0400

    Fixed dark mode

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, RendererFactory2, Inject, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,17 @@ export class ThemeService {
 
   renderer: Renderer2;
 
-  constructor(private rendererFactory: RendererFactory2, @Inject(DOCUMENT) private document: Document) { 
+  constructor(private rendererFactory: RendererFactory2, @Inject(DOCUMENT) private document: Document, private statusBar: StatusBar) { 
     this.renderer = this.rendererFactory.createRenderer(null, null);
   }
 
   enableDark() {
     this.renderer.addClass(this.document.body, 'dark')
+    this.statusBar.styleBlackOpaque();
   }
 
   enableLight() {
     this.renderer.removeClass(this.document.body, 'dark')
+    this.statusBar.styleDefault();
   }
 }
