2b48d9dc3337c6711c28ef188d47c14fde8b2fd1
Miro Manestar <kinectechx@gmail.com>
2019-10-14 13:45:15 -0400

    Possible keyboard fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -6,12 +6,13 @@ import { ThemeService } from './services/theme.service';
 import { StorageService } from './services/storage.service';
 
 declare var Keyboard: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -65,5 +66,42 @@ export class AppComponent {
       Keyboard.disableScroll(true)
       Keyboard.hideFormAccessoryBar(false)
     });
+  }
+
+  ngAfterViewInit() {
+    // This element never changes.
+    let ionapp = document.getElementsByTagName("ion-app")[0];
+
+    window.addEventListener('keyboardDidShow', async (event) => {
+        // Move ion-app up, to give room for keyboard
+        let kbHeight: number = event["keyboardHeight"];
+        let viewportHeight: number = $(window).height();
+        let inputFieldOffsetFromBottomViewPort: number = viewportHeight - $(':focus')[0].getBoundingClientRect().bottom;
+        let inputScrollPixels = kbHeight - inputFieldOffsetFromBottomViewPort;
+
+        // Set margin to give space for native keyboard.
+        ionapp.style["margin-bottom"] = kbHeight.toString() + "px";
+
+        // But this diminishes ion-content and may hide the input field...
+        if (inputScrollPixels > 0) {
+            // ...so, get the ionScroll element from ion-content and scroll correspondingly
+            // The current ion-content element is always the last. If there are tabs or other hidden ion-content elements, they will go above.
+            let ionScroll = await $("ion-content").last()[0].getScrollElement();
+            setTimeout(() => {
+                $(ionScroll).animate({
+                    scrollTop: ionScroll.scrollTop + inputScrollPixels
+                }, 300);
+            }, 300); // Matches scroll animation from css.
+        }
+    });
+    window.addEventListener('keyboardDidHide', () => {
+        // Move ion-app down again
+        // Scroll not necessary.
+        ionapp.style["margin-bottom"] = "0px";
+    });
+  }
+
+  ngOnInit() {
+
   }
 }
