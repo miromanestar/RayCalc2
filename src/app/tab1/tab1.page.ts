55a5dd3b222d343d07d5289ad05a7df71f1c3d3c
Miro Manestar <miroimanestar@msn.com>
2020-06-04 15:00:21 -0400

    Added user agreement

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 import { StorageService } from '../services/storage.service';
 import { SettingsPage } from '../settings/settings.page';
 import { ThemeService } from '../services/theme.service';
@@ -14,7 +14,7 @@ export class Tab1Page {
 
   date = new Date();
   
-  constructor(public router: Router, public modalController : ModalController, private theme: ThemeService, private storageService: StorageService) {}
+  constructor(public router: Router, public modalController : ModalController, private theme: ThemeService, private storageService: StorageService, public alertController: AlertController) {}
 
   async presentModal() {
     const modal = await this.modalController.create({
@@ -23,8 +23,22 @@ export class Tab1Page {
     return await modal.present();
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'User Agreement',
+      message: 'By using this app, you agree that this app is experimental, and that it is not to be used in any official capacity whatsoever.',
+      buttons: ['Agree']
+    });
+    return await alert.present();
+    let userAgr = await alert.onDidDismiss();
+  }
+
   goToInputPage(calcType: String) {
     const navigationExtras : NavigationExtras = { state: { calcSelect: calcType }};
     this.router.navigate(['/tabs/tab1/input-page'], navigationExtras);
+  }
+
+  ngOnInit() {
+    this.presentAlert();
   }
 }
