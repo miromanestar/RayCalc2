3e8dc7e369016f11c1976d44c4c7b79533d48feb
Miro Manestar <miroimanestar@gmail.com>
2020-06-14 13:48:03 -0400

    Fixed NSURLERROR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,7 +51,7 @@ export class AppComponent implements OnInit {
         if(theme == 'light') { this.enableLight() }
         if(theme == 'dark') { this.enableDark() }
       }
-    })
+    });
   }
 
   enableDark() {
@@ -69,6 +69,7 @@ export class AppComponent implements OnInit {
       StatusBar.show();
       this.splashScreen.hide();
 
+      /*
       window.addEventListener("keyboardWillShow", function (evt) {
         $(".keyboard-spacer").height(evt.keyboardHeight * .9);
         $(".keyboard-spacer").show();
@@ -77,7 +78,7 @@ export class AppComponent implements OnInit {
       window.addEventListener("keyboardWillHide", function (evt) {
         $(".keyboard-spacer").height(0);
         $(".keyboard-spacer").hide();
-      });
+      });*/
 
       Keyboard.setAccessoryBarVisible({ isVisible: true });
       //Keyboard.setResizeMode({ mode: KeyboardResize.Ionic });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { environment } from '../environments/environment';
     {
       scrollPadding: false,
       scrollAssist: false,
-      swipeBackEnabled: false, //CHANGE TO TRUE FOR iOS BUILDS, FALSE FOR WEB BUILDS
+      swipeBackEnabled: true, //CHANGE TO TRUE FOR iOS BUILDS, FALSE FOR WEB BUILDS
     }),
     AppRoutingModule, SettingsPageModule, ResultsPageModule, IonicStorageModule.forRoot(), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
